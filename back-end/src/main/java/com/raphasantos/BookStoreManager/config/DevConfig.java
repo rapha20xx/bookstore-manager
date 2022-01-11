e761840324be5454a9c5b99e146b3760189e0174
Raphael Santos <raphaelsantos0712@gmail.com>
2022-01-11 14:03:54 -0300

    Dev profile and Database setup

---
+++ back-end/src/main/java/com/raphasantos/BookStoreManager/config/DevConfig.java
@@ -0,0 +1,27 @@
+package com.raphasantos.BookStoreManager.config;
+
+import com.raphasantos.BookStoreManager.services.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    DbService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiatingDb() {
+        if (strategy.equals("create")){
+            this.dbService.instantiatingDbService();
+        }
+        return false;
+    }
+}
